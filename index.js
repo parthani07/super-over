f77d9dd467a1b142da72122d6372b35e390fea44
parthani07 <parthani07@gmail.com>
2022-10-19 15:57:35 +0000

    super-over

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div class="bg">
+    <h1 class="heading">Super Over League</h1>
+    <div class="image-card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
